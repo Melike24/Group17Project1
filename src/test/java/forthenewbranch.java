79bd96aee12b9128241795f2bf4d858082176c39
Selma-Atabas <68811734+Selma-Atabas@users.noreply.github.com>
2020-10-17 14:19:54 +0200

    Update forthenewbranch.java

--- src/test/java/forthenewbranch.java
+++ src/test/java/forthenewbranch.java
@@ -1,5 +1,5 @@
 public class forthenewbranch {
     public static void main(String[] args) {
-        System.out.println("hellooooooooooooooooooooooooooooo");
+        System.out.println("helloooooooooooooooooooo");
     }
 }
