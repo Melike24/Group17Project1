07c821924d5185fbe7758f5a82e5cd29bce17f8e
Nese-yigit <nese1988.ny@gmail.com>
2020-10-17 13:43:09 +0200

    i added 2 lines

--- src/test/java/hello.java
+++ src/test/java/hello.java
@@ -1,5 +1,11 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello guys");
+
+        System.out.println("Hello guys again ");
+
+
+        System.out.println("hi guys");
+      
     }
 }
