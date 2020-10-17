bc127e8cbaa89ae888695880f61cf4482756ed80
maria-badanina <maria.badanina.s@gmail.com>
2020-10-17 14:05:24 +0200

    Try to make a conflict

--- src/test/java/hello.java
+++ src/test/java/hello.java
@@ -2,7 +2,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Hello guys");
 
-        System.out.println("Hello guys again ");
+        System.out.println("Try to make a conflict!");
 
     }
 }
