8e71c6c6f10c4b696a0f57412de9af0f0d41171d
Maria Badanina <maria.badanina.s@gmail.com>
2020-10-17 14:12:40 +0200

    Update testMaria.java

--- src/test/java/testMaria.java
+++ src/test/java/testMaria.java
@@ -3,6 +3,7 @@ public class testMaria {
     public static void main(String[] args) {
 
         System.out.println("Hi \uD83D\uDC4B");
+        System.out.println("Hi again \uD83D\uDC4B \uD83D\uDC4B");
 
     }
 
