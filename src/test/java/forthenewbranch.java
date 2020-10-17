77ddad6a01759a248580fe58d520922972af6e19
Selma-Atabas <selma-aykal43@hotmail.com>
2020-10-17 14:20:23 +0200

    some more lines

--- src/test/java/forthenewbranch.java
+++ src/test/java/forthenewbranch.java
@@ -1,5 +1,6 @@
 public class forthenewbranch {
     public static void main(String[] args) {
         System.out.println("hellooooooooooooooooooooooooooooo");
+        System.out.println("jcnwhfiuhkdjnksdlnv");
     }
 }
