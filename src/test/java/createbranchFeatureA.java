bb7374b0eea9a3b4e8e6d07ce1e8edc679970c77
maria-badanina <maria.badanina.s@gmail.com>
2020-10-17 14:08:37 +0200

    Try to make a conflict again

--- src/test/java/createbranchFeatureA.java
+++ src/test/java/createbranchFeatureA.java
@@ -1,5 +1,6 @@
 public class createbranchFeatureA {
     public static void main(String[] args) {
         System.out.println("Hello  dears");
+        System.out.println("Try to make a conflict!");
     }
 }
